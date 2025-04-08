4e57ed8c69b93538eec10128a51735d13769c08d
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2025-04-08 14:18:26 +0300

    region button

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,20 +1,24 @@
 import React, { createContext, useState, useEffect } from 'react'
 
-export const DataContext = createContext()
+
+const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
-  const [data, setData] = useState([])
+  const [data, updateData] = useState([])
+  // useEffect(() => {
+  //   fetch('https://restcountries.com/v3.1/all')
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setData(json)
+  //     })
+  //     .catch((err) => {
+  //       console.error('Error fetching countries:', err)
+  //     })
+  // }, [])
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json)
-      })
-      .catch((err) => {
-        console.error('Error fetching countries:', err)
-      })
-  }, [])
+  const setData = (newData) => {
+    updateData((newData))
+  }
 
   return (
     <DataContext.Provider value={{ data, setData}}>
@@ -22,3 +26,5 @@ export const DataProvider = ({ children }) => {
     </DataContext.Provider>
   );
 };
+
+export default DataContext;
