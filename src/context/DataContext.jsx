c8383db4ca3f3395e91d1abf7a22eb297c9a58ab
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2025-04-08 14:19:56 +0300

    region button

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -5,17 +5,7 @@ const DataContext = createContext()
 
 export const DataProvider = ({ children }) => {
   const [data, updateData] = useState([])
-  // useEffect(() => {
-  //   fetch('https://restcountries.com/v3.1/all')
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setData(json)
-  //     })
-  //     .catch((err) => {
-  //       console.error('Error fetching countries:', err)
-  //     })
-  // }, [])
-
+  
   const setData = (newData) => {
     updateData((newData))
   }
