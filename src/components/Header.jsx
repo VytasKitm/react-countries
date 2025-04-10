20e1448dc2304256c563dfe0ecf66591f6d66176
EgidijusPak <pakalnisegidijus@gmail.com>
2025-04-10 16:07:50 +0300

    Added all countries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { RegionButton } from './RegionButton'
-import { SearchComponent } from './SearchComponent'
+import React from "react";
+import { RegionButton } from "./RegionButton";
+import { SearchComponent } from "./SearchComponent";
+import { AllCountriesButton } from "./AllCountriesButton";
 
 export const Header = () => {
   return (
-    <div className='header'>
-        <RegionButton region={"Asia"}/>
-        <RegionButton region={"Europe"}/>
-        <RegionButton region={"Africa"}/>
-        <RegionButton region={"Oceania"}/>
-        <RegionButton region={"North America"}/>
-        <RegionButton region={"Antarctica"}/>
-        <RegionButton region={"South America"}/>
-        <SearchComponent/>
+    <div className="header">
+      <AllCountriesButton />
+      <RegionButton region={"Asia"} />
+      <RegionButton region={"Europe"} />
+      <RegionButton region={"Africa"} />
+      <RegionButton region={"Oceania"} />
+      <RegionButton region={"North America"} />
+      <RegionButton region={"Antarctica"} />
+      <RegionButton region={"South America"} />
+      <SearchComponent />
     </div>
-  )
-}
+  );
+};
