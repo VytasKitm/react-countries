945297d810f4e32d88196ed650b28e52de13db03
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2025-04-10 13:50:31 +0300

    searchComponent fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import "./App.css";
-import { getAllData } from "./services/getAllData";
-import { getRegionData } from "./services/getRegionData";
 import { RegionButton } from "./components/RegionButton";
 import { DataProvider } from "./context/DataContext";
 import { CountryInfo} from "./components/CountryInfo"
 import { Card } from "./components/Card"
+import { SearchComponent } from "./components/SearchComponent";
 
 function App() {
 
@@ -18,6 +17,7 @@ function App() {
           <DataProvider>
                       <RegionButton region={"europe"}/>
                       <RegionButton region={"asia"}/>
+                      <SearchComponent/>
                       <Card/>
                       <Card/>
           </DataProvider>
