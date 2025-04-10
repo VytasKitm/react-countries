335577377ad8589004648a65b3d0559870d15a0a
EgidijusPak <pakalnisegidijus@gmail.com>
2025-04-10 17:08:59 +0300

    styliukas

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style.css";
 import { RegionButton } from "./components/RegionButton";
 import { DataProvider } from "./context/DataContext";
-import { CountryInfo} from "./components/CountryInfo"
+import { CountryInfo } from "./components/CountryInfo";
 import { SearchComponent } from "./components/SearchComponent";
 import { Main } from "./components/Main";
 import { Header } from "./components/Header";
 
-
 function App() {
-
-  return <>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <DataProvider>
-                      <Header/>
-                      <Main/>
-          </DataProvider>
-        } />
-        <Route path="/details/:name" element={<CountryInfo/>} />
-      </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DataProvider>
+                <Header className="header" />
+                <Main className="main" />
+              </DataProvider>
+            }
+          />
+          <Route path="/details/:name" element={<CountryInfo />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
